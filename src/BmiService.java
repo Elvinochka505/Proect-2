357a65b8869572513b7a001372d204f4e78b2d2e
Elvina <khabibullinaelvina@mail.ru>
2025-03-16 21:05:00 +0500

    commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public int calculate (int weightKg,double heightM) {
-        double Bmi = weightKg/(heightM*heightM);
-    return (int) Bmi;
+    public int calculate(int weightKg, double heightM) {
+        double Bmi = weightKg / (heightM * heightM);
+        return (int) Bmi;
 
 
-}
+    }
 }
