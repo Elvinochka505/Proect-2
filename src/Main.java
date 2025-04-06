1dd908403591afd86ea6a35b655765c6d3e4b191
Elvina <khabibullinaelvina@mail.ru>
2025-04-06 14:42:01 +0500

    the code is formatted

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weightKg = 65;
+        int weightKg = 63;
         double heightM = 1.62;
         int Bmi = service.calculate(weightKg, heightM);
         System.out.println(Bmi);
